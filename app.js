f961abab88bf51639025e59b919b5f0d00292858
fd <fede.atanasoff@gmail.com>
2018-07-26 22:05:53 -0300

    agregando colores a la salida por consola con el paquete colors

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const { crearArchivo, listarTabla } = require("./multiplicar/multiplicar.js");
 // const argv = require("./config/yargs").argv;
 const { argv } = require("./config/yargs");
+const colors = require("colors");
 
 //console.log(argv);
 let comando = argv._[0];
-console.log("comando pasado por argumento: ", comando);
+// console.log("comando pasado por argumento: ", comando);
 
 switch (comando) {
   case "crear":
     crearArchivo(argv.base, argv.limite)
       .then(archivo => {
-        console.log(`El archivo ${archivo} ha sido creado`);
+        console.log(`Archivo creado: ${archivo}`);
       })
       .catch(err => {
         console.log("Error: ", err);
--- multiplicar/multiplicar.js
+++ multiplicar/multiplicar.js
@@ -1,8 +1,13 @@
 const fs = require("fs");
+const colors = require("colors");
 
 let listarTabla = (base, limite) => {
+  console.log("=============================".bold.green);
+  console.log(`======== Tabla del ${base} ========`.bold.green);
+  console.log("=============================".bold.green);
+
   for (let i = 0; i <= limite; i++) {
-    console.log(`${base} * ${i} = ${base * i}`);
+    console.log(` ${base} * ${i} = ${base * i} `.bold);
   }
 };
 
@@ -21,7 +26,7 @@ let crearArchivo = (base, limite = 4) => {
 
     fs.writeFile(`tablas/tabla-del-${base}-al-${limite}.txt`, tabla, err => {
       if (err) reject(err);
-      else resolve(`tabla-del-${base}-al-${limite}.txt`);
+      else resolve(`tabla-del-${base}-al-${limite}.txt`.bold);
     });
   });
 };
